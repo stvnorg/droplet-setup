0652802eab7bcd2f354e7523c24e06a41534448f
stvnorg <stvn.org@gmail.com>
2017-05-29 17:16:31 +0800

    2nd Commit

--- droplet/api/fabfile.py
+++ droplet/api/fabfile.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.operations import *
 from fabric.contrib.console import confirm
+import os
 
 env.hosts = ['172.168.2.115']
 env.user = 'root'
@@ -16,7 +17,11 @@ def install():
     run('apt-get install -y php5 php5-curl php5-mysqlnd apache2 zip')    
     run('a2enmod rewrite')
     run('service apache2 restart')
+    run('cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak')
+    local_path = os.getcwd() + '/000-default.conf'
+    remote_path = '/etc/apache2/sites-available/000-default.conf'
     with settings(warn_only=True):
-        result = run('cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak')
+        result = put(local_path, remote_path, mode=0755)
     if result.failed and not confirm("Apache setup failed. Continue anyway?"):
         abort("Aborting at user request.")
+    run('service apache2 restart')
