6dea754b2864eae788196492e3846430c79019f8
stvnorg <stvn.org@gmail.com>
2017-05-29 17:38:58 +0800

    3rd Commit

--- droplet/web/fabfile.py
+++ droplet/web/fabfile.py
@@ -4,20 +4,15 @@ from fabric.operations import *
 from fabric.contrib.console import confirm
 import os
 
-env.hosts = ['172.168.2.115']
+env.hosts = ['172.168.2.116']
 env.user = 'root'
 env.password = '123456'
 
-
-def hello(name):
-    print ("Hello " + name + "!")
-
 def install():
-    run('uname -a')
-    run('apt-get install -y php5 php5-curl php5-mysqlnd apache2 zip')    
-    run('a2enmod rewrite')
-    run('service apache2 restart')
-    run('cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak')
+    run('apt-get install -y php5')    
+    run('apt-get remove -y apache2')
+    run('apt-get install -y nginx')
+    run('apt-get install -y php5-curl php5-gd php5-mysql php5-fpm zip')
     local_path = os.getcwd() + '/000-default.conf'
     remote_path = '/etc/apache2/sites-available/000-default.conf'
     with settings(warn_only=True):
@@ -25,4 +20,3 @@ def install():
     if result.failed and not confirm("Apache setup failed. Continue anyway?"):
         abort("Aborting at user request.")
     run('service apache2 restart')
-    run('timedatectl set-timezone Asia/Jakarta')
